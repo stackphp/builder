7a447994df946e68014d3284c26b407882f4c4f5
Saif Eddin Gmati <azjezz@azjezz.me>
2018-07-15 19:05:29 +0100

    Update StackedHttpKernelTest.php

--- tests/unit/Stack/StackedHttpKernelTest.php
+++ tests/unit/Stack/StackedHttpKernelTest.php
@@ -6,8 +6,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\TerminableInterface;
+use PHPUnit\Framework\TestCase;
 
-class StackedHttpKernelTest extends \PHPUnit_Framework_TestCase
+class StackedHttpKernelTest extends TestCase
 {
     /** @test */
     public function handleShouldDelegateToApp()
