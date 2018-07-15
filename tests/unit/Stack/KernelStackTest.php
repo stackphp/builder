994b5a74d4591168dc6f91958d32baac497d1c80
Saif Eddin Gmati <azjezz@azjezz.me>
2018-07-15 18:40:40 +0100

    Update KernelStackTest.php

--- tests/unit/Stack/KernelStackTest.php
+++ tests/unit/Stack/KernelStackTest.php
@@ -14,5 +14,53 @@ class KernelStackTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function withoutMiddlewaresItShouldReturnOriginalResponse()
     {
+        $app = $this->getHttpKernelMock($response = new Response('ok'));
+        
+        $kernel = new KernelStack($app);
+        $request = Request::create('/');
+        $response = $kernel->handle($request);
+        
+        $this->assertSame('ok', $response->getContent());
+    }
+    
+    /** @test */
+    public function appendMiddlewareShouldAppendToBody()
+    {
+        $app = $this->getHttpKernelMock(new Response('ok'));
+        
+        $kernel = new KernelStack($app);
+        $kernel->push('Stack\AppendA');
+        $request = Request::create('/');
+        $response = $kernel->handle($request);
+        
+        $this->assertSame('ok.A', $response->getContent());
+    }
+    
+    /** @test */
+    public function pushShouldReturnSelf()
+    {
+        $app = $this->getHttpKernelMock(new Response(''));
+        $kernel = new KernelStack($app);
+        $this->assertSame($kernel, $kernel->push('Stack\AppendA'));
+    }
+    
+    /** @test */
+    public function unshiftShouldReturnSelf()
+    {
+        $app = $this->getHttpKernelMock(new Response(''));
+        $kernel = new KernelStack($app);
+        $this->assertSame($kernel, $kernel->unshift('Stack\AppendA'));
+    }
+    
+    private function getHttpKernelMock(Response $response)
+    {
+        $app = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
+        $app->expects($this->any())
+            ->method('handle')
+            ->with($this->isInstanceOf('Symfony\Component\HttpFoundation\Request'))
+            ->will($this->returnValue($response));
+        return $app;
     }
 }
+
+class_exists(BuilderTest::class);
